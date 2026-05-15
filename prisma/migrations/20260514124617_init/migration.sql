fcd477bbd1b158af281f8498d4a4b9eb043b798c
oyilmaztekin <yilmaztekin.ozer@gmail.com>
2026-05-15 12:32:24 +0300

    refactor: remove migrations from .gitignore

---
+++ prisma/migrations/20260514124617_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Horse" (
+    "number" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "condition" INTEGER NOT NULL
+);
---
+++ prisma/migrations/20260515092416_add_app_state/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "AppState" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT DEFAULT 1,
+    "restingUntil" DATETIME
+);
